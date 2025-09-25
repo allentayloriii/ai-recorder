12231b59d0daa2433b3c0ef02d087c9c88c86ed9
SoundtrkSamurai <furtimat3@yahoo.com>
2025-09-24 18:29:49 -0700

    Update expo-sqlite to version 16.0.8 and enhance middleware for COEP and COOP headers

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,16 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts.push("sql");
 
+// Add wasm asset support
+config.resolver.assetExts.push("wasm");
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    middleware(req, res, next);
+  };
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
