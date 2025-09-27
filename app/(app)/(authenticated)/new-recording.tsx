0cd79c7c7d448442d052e9bd86410ebf9a95f280
SoundtrkSamurai <furtimat3@yahoo.com>
2025-09-26 20:39:33 -0700

    Refactor NewRecording component layout; enhance input fields and save functionality

--- app/(app)/(authenticated)/new-recording.tsx
+++ app/(app)/(authenticated)/new-recording.tsx
@@ -2,7 +2,7 @@ import { NewNote } from "@/db/schema";
 import { useNotes } from "@/providers/NoteProvider";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const NewRecording = () => {
   const { uri } = useLocalSearchParams<{ uri: string }>();
@@ -28,16 +28,40 @@ const NewRecording = () => {
       note_audio: uri || "",
       created_at: new Date().toISOString(),
     };
+
+    await saveNote(note);
+    router.dismissAll();
   };
   return (
-    <View style={styles.container}>
-      <Text>NewRecording</Text>
+    <View className="min-h-full p-4">
+      <TextInput
+        value={title}
+        onChangeText={setTitle}
+        placeholder="Title"
+        textAlignVertical="top"
+        className="p-2 mb-4 text-base bg-white border border-gray-300 rounded-lg"
+      />
+      <TextInput
+        value={transcription}
+        onChangeText={setTranscription}
+        placeholder="Transcription will appear here..."
+        multiline
+        textAlignVertical="top"
+        className="p-2 mb-4 text-base bg-white border border-gray-300 rounded-lg h-[200px]"
+      />
+      <TouchableOpacity
+        onPress={handleSave}
+        disabled={isLoading}
+        className={`p-4 bg-blue-500 rounded-lg ${
+          isLoading ? "opacity-50" : ""
+        }`}
+      >
+        <Text className="font-medium text-center text-white">
+          Save Transcription
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default NewRecording;
-
-const styles = StyleSheet.create({
-  container: {},
-});
